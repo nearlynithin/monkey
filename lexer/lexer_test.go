f213167c0ccf6363da69655ced8033b47648cefb
nearlynithin <nearlynithin@gmail.com>
2024-11-06 20:29:55 +0530

    larger test case

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -11,22 +11,57 @@ import (
 
 
 func TestNextToken(t *testing.T) {
-	input := `=+{}(),;`
+	input := `let eight = 8;
+	let ten = 10;
+	let add = fn(x,y) {
+	  x+y;
+	};
+	
+	let result = add(five, ten);`
 
 	tests := []struct {
-		expectedType token.TokenType
+		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "eight"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
+		{token.INT, "8"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
+		{token.IDENT, "x"},
 		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
+	
 
 	l := New(input)
 
@@ -36,7 +71,7 @@ func TestNextToken(t *testing.T) {
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong, expected=%q, got=%q",
 				i,tt.expectedType,tok.Type)
-		}
+		}	
 		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong, expected=%q, got=%q",
 				i,tt.expectedLiteral,tok.Literal)
