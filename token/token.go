4f1c831254cb6efb8c93e7c70d885a1e6704a076
nearlynithin <nearlynithin@gmail.com>
2024-11-06 21:20:50 +0530

    feat: added single space operators and keywords like if,else,return,true,false

--- token/token.go
+++ token/token.go
@@ -19,6 +19,12 @@ const (
 	//operators
 	ASSIGN = "="
 	PLUS = "+"
+	SLASH = "/"
+	BANG = "!"
+	MINUS = "-"
+	ASTERISK = "*"
+	LT = "<"
+	GT = ">"
 
 	//delimiters
 	COMMA = ","
@@ -31,12 +37,21 @@ const (
 	//keywords
 	FUNCTION = "FUNCTION"
 	LET = "LET"
-	
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
 )
 
 var keywords = map[string]TokenType {
 	"fn" : FUNCTION,
 	"let" : LET,
+	"if" : IF,
+	"else" : ELSE,
+	"true" : TRUE,
+	"false" : FALSE,
+	"return" : RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
