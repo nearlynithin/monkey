cede13c59672b30284bc7bf30a655f2ab48cb368
nearlynithin <nearlynithin@gmail.com>
2024-11-06 20:28:00 +0530

    feat: added readIdentifier and readNumber methods. Skip whitespace method to ignore spaces

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -37,6 +37,8 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 func (l* Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhiteSpace()
+
 	switch l.ch {
 	case '=' :
 		tok = newToken(token.ASSIGN, l.ch)
@@ -57,7 +59,50 @@ func (l* Lexer) NextToken() token.Token {
 	case 0: //This represents ASCII for null value
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL,l.ch)
+		}
 	}
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+
+func isLetter(ch byte) bool {
+	return 'a'<= ch && ch <= 'z' || 'A' <= ch && 'Z' <= ch || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) skipWhiteSpace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
