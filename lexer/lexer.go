b28c51c53c58e78e7174170e1b223e396eeeb98b
nearlynithin <nearlynithin@gmail.com>
2024-11-07 06:11:40 +0530

    fix: Capital letters identifiers bug fix

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -126,7 +126,7 @@ func (l *Lexer) readNumber() string {
 
 
 func isLetter(ch byte) bool {
-	return 'a'<= ch && ch <= 'z' || 'A' <= ch && 'Z' <= ch || ch == '_'
+	return 'a'<= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
 func isDigit(ch byte) bool {
