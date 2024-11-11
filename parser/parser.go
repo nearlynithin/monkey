35213b869ed995ebc7a28a852c81c5879afabc2b
nearlynithin <nearlynithin@gmail.com>
2024-11-11 20:12:38 +0530

    feat: errors field + peekError function to check for errors

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/nearlynithin/monkey/ast"
 	"github.com/nearlynithin/monkey/lexer"
 	"github.com/nearlynithin/monkey/token"
@@ -12,10 +14,14 @@ type Parser struct {
 	l *lexer.Lexer
 	curToken token.Token
 	peekToken token.Token
+	errors[] string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l: l,
+		errors: []string{},
+	}
 
 	//read token twice, first time peekToken is set, next curToken is set 
 	p.nextToken()
@@ -86,6 +92,17 @@ func (p *Parser) expectPeek( t token.TokenType) bool {
 		p.nextToken() // if got the expected token, update current token immediately
 		return true
 	} else {
+
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",t,p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
