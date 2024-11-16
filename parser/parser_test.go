4f662fe364c33608e7d0bd482f1fc619d1efa58a
nearlynithin <nearlynithin@gmail.com>
2024-11-16 18:12:35 +0530

    feat: Tests for Boolean parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -176,10 +176,12 @@ func TestParsingPrefixExpressions( t *testing.T ) {
 	prefixTests := []struct {
 		input string
 		operator string
-		intergerValue int64
+		intergerValue interface{}
 	}{
 		{"!5","!",5},
 		{"-15","-",15},
+		{"!true","!",true},
+		{"!false","!",false},
 	}
 
 	for _, tt := range prefixTests {
@@ -206,7 +208,7 @@ func TestParsingPrefixExpressions( t *testing.T ) {
 			t.Fatalf("exp.Operator is not '%s', got=%s",tt.operator,exp.Operator)
 		}
 
-		if !testIntegerLiteral(t,exp.Right, tt.intergerValue) {
+		if !testLiteralExpression(t,exp.Right, tt.intergerValue) {
 			return
 		}
 	}
@@ -234,9 +236,9 @@ func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
 func TestParsingInfixExpressions(t *testing.T) {
 	infixTests := []struct{
 		input string
-		leftValue int64
+		leftValue interface{}
 		operator string
-		rightValue int64
+		rightValue interface{}
 	}{
 		{"5+5",5,"+",5},
 		{"5-5",5,"-",5},
@@ -245,6 +247,9 @@ func TestParsingInfixExpressions(t *testing.T) {
 		{"5<5",5,"<",5},
 		{"5==5",5,"==",5},
 		{"5!=5",5,"!=",5},
+		{"true == true",true,"==",true},
+		{"true != false",true,"!=",false},
+		{"false == false",false,"==",false},
 	}
 
 	for _,tt := range infixTests {
@@ -267,7 +272,7 @@ func TestParsingInfixExpressions(t *testing.T) {
 			t.Fatalf("exp is not *ast.InfixExpression, got=%T",stmt.Expression)
 		}
 
-		if !testIntegerLiteral(t, exp.Left, tt.leftValue) {
+		if !testLiteralExpression(t, exp.Left, tt.leftValue) {
 			return
 		}
 
@@ -275,7 +280,7 @@ func TestParsingInfixExpressions(t *testing.T) {
 			t.Fatalf("exp.Operator is not %s, got=%s",tt.operator, exp.Operator)
 		}
 
-		if !testIntegerLiteral(t,exp.Right, tt.rightValue){
+		if !testLiteralExpression(t,exp.Right, tt.rightValue){
 			return
 		}
 	}
@@ -338,6 +343,22 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 			"3 + 4 * 5 == 3 * 1 + 4 * 5",
 			"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
 		},
+		{
+			"true",
+			"true",
+		},
+		{
+			"false",
+			"false",
+		},
+		{
+			"3>5 == false",
+			"((3 > 5) == false)",
+		},
+		{
+			"3<5 == true",
+			"((3 < 5) == true)",
+		},
 	}
 
 	for _, tt := range tests {
@@ -380,6 +401,8 @@ func testLiteralExpression(t * testing.T, exp ast.Expression, expected interface
 		return testIntegerLiteral(t,exp,v)
 	case string:
 		return testIdentifier(t,exp,v)
+	case bool:
+		return testBooleanLiteral(t,exp,v)
 	}
 	t.Errorf("type of exp is not handled yet, got=%T",exp)
 	return false
@@ -403,3 +426,56 @@ func testInfixExpression(t *testing.T, exp ast.Expression, left interface{}, ope
 	}
 	return true
 }
+
+func TestBooleanExpression(t *testing.T) {
+	tests := []struct {
+		expectedLiteral string
+		expectedValue   bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for i, tt := range tests {
+		l := lexer.New(tt.expectedLiteral)
+		p := New(l)
+		program := p.ParseProgram()
+	
+		stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+		if !ok {
+			t.Fatalf("program.Statements[%d] is not *ast.ExpressionStatement, got=%T", i, program.Statements[i])
+		}
+
+		boolean, ok := stmt.Expression.(*ast.Boolean)
+		if !ok {
+			t.Fatalf("stmt.Expression is not *ast.Boolean, got=%T", stmt.Expression)
+		}
+
+		if boolean.TokenLiteral() != tt.expectedLiteral {
+			t.Errorf("boolean.TokenLiteral() is not %q, got=%q", tt.expectedLiteral, boolean.TokenLiteral())
+		}
+
+		if boolean.Value != tt.expectedValue {
+			t.Errorf("boolean.Value is not %t, got=%t", tt.expectedValue, boolean.Value)
+		}
+	}
+}
+
+func testBooleanLiteral(t *testing.T, exp ast.Expression, value bool ) bool {
+	bo, ok := exp.(*ast.Boolean)
+	if !ok {
+		t.Errorf("exp not *ast.Boolean, got=%T",exp)
+		return false
+	}
+
+	if bo.Value != value {
+		t.Errorf("bo.Value not %t, got=%t", value,bo.Value)
+		return false
+	}
+
+	if bo.TokenLiteral() != fmt.Sprintf("%t",value) {
+		t.Errorf("bo.TokenLiteral() not %T, got=%T",value,bo.TokenLiteral())
+		return false
+	}
+	return true
+}true
