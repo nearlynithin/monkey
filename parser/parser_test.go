b3b9c3f79b1bf8d5562f4d53a00e8d5701adf5f1
nearlynithin <nearlynithin@gmail.com>
2024-11-23 07:17:05 +0530

    feat: Tests for parsing If expression and if else expressions

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -499,3 +499,103 @@ func testBooleanLiteral(t *testing.T, exp ast.Expression, value bool ) bool {
 	}
 	return true
 }
+
+
+func TestIfExpression(t *testing.T) {
+	input := "if (x < y) { x }"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t,p)
+
+	if len(program.Statements) != 1 {
+		t.Errorf("program body does not contain %d statements. got=%d\n",1,len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Errorf("program.Statements[0] is not *ast.ExpressionStatement, got=%T",program.Statements[0])
+	}
+
+	exp, ok := stmt.Expression.(*ast.IfExpression)
+	if !ok {
+		t.Errorf("stmt.Expression is not *ast.IfExpression, got=%T",stmt.Expression)
+	}
+
+	if !testInfixExpression(t,exp.Condition,"x","<","y") {
+		t.Errorf("exp.Condition does not contain 'x < y'")
+		return
+	}
+
+	if len(exp.Consequence.Statements) != 1 {
+		t.Errorf("exp.Consequence.Statements is not 1. got=%d\n",len(exp.Consequence.Statements))
+	}
+
+	consequence , ok := exp.Consequence.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Errorf("exp.Consequence.Statements[0] is not *ast.ExpressionStatement, got=%T",exp.Consequence.Statements[0])
+	}
+
+	if !testIdentifier(t,consequence.Expression,"x") {
+		return
+	}
+
+	if exp.Alternative != nil {
+		t.Errorf("exp.Alternative.Statements is not nil. got=%+v",exp.Alternative)
+	}
+}
+
+func TestIfElseExpression(t *testing.T) {
+	input := "if (x < y) { y } else { x }"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t,p)
+
+	if len(program.Statements) != 1 {
+		t.Errorf("program body does not contain %d statements. got=%d\n",1,len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Errorf("program.Statements[0] is not *ast.ExpressionStatement, got=%T",program.Statements[0])
+	}
+
+	exp, ok := stmt.Expression.(*ast.IfExpression)
+	if !ok {
+		t.Errorf("stmt.Expression is not *ast.IfExpression, got=%T",stmt.Expression)
+	}
+
+	if !testInfixExpression(t,exp.Condition,"x","<","y") {
+		t.Errorf("exp.Condition does not contain 'x < y'")
+		return
+	}
+
+	if len(exp.Consequence.Statements) != 1 {
+		t.Errorf("exp.Consequence.Statements is not 1. got=%d\n",len(exp.Consequence.Statements))
+	}
+
+	consequence , ok := exp.Consequence.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Errorf("exp.Consequence.Statements[0] is not *ast.ExpressionStatement, got=%T",exp.Consequence.Statements[0])
+	}
+
+	if !testIdentifier(t,consequence.Expression,"y") {
+		return
+	}
+
+	if len(exp.Alternative.Statements) != 1 {
+		t.Errorf("len(exp.Alternative.Statements) is not 1, got=%d",len(exp.Alternative.Statements))
+	}
+
+	alternative , ok := exp.Alternative.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Errorf("exp.Alternative.Statements[0] is not *ast.ExpressionStatement, got=%T",exp.Alternative.Statements[0])
+	}
+
+	if !testIdentifier(t,alternative.Expression,"x") {
+		return
+	}
+}
