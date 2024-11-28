7307b1556d8d56f92b1c037213e605e47dd48bbb
nearlynithin <nearlynithin@gmail.com>
2024-11-28 17:17:03 +0530

    feat: Tests for parsing function literals

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -599,3 +599,74 @@ func TestIfElseExpression(t *testing.T) {
 		return
 	}
 }
+
+func TestFunctionLiteralParsing(t * testing.T) {
+	input := "fn (x,y) { x + y}"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t,p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("len(program.Statements) is not 1, got=%d",len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not *ast.ExpressionStatement, got=%T",program.Statements[0])
+	}
+
+	function, ok := stmt.Expression.(*ast.FunctionLiteral)
+	if !ok {
+		t.Errorf("stmt.Expression is not *ast.FunctionLiteral, got=%T",stmt.Expression)
+	}
+
+	if len(function.Parameters) != 2 {
+		t.Fatalf("len(function.Parameters) is not 2, got=%T",len(function.Parameters))
+	}
+	testLiteralExpression(t,function.Parameters[0],"x")
+	testLiteralExpression(t,function.Parameters[1],"y")
+
+	if len(function.Body.Statements) != 1 {
+		t.Errorf("len(function.Body.Statements) is not 1, got=%T",len(function.Body.Statements))
+	}
+
+	bodyStmt, ok := function.Body.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("function.Body.Statements[0] is not *ast.ExpressionStatement, got=%T",function.Body.Statements[0])
+	}
+
+	testInfixExpression(t,bodyStmt.Expression,"x","+","y")
+}
+
+
+func TestFunctionParameterParsing(t * testing.T) {
+	tests := []struct{
+		input string
+		expectedParams []string
+	}{
+		{input: "fn(){ };",expectedParams: []string{}},
+		{input: "fn(x) {};",expectedParams: []string{"x"}},
+		{input: "fn(x,y){}",expectedParams: []string{"x","y"}},
+	}
+
+	for _,tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t,p)
+
+		stmt := program.Statements[0].(*ast.ExpressionStatement)
+		function := stmt.Expression.(*ast.FunctionLiteral)
+
+		if len(function.Parameters) != len(tt.expectedParams) {
+			t.Errorf("Length of the parameters is wrong, got=%d",len(function.Parameters))
+		}
+
+		for i,ident := range tt.expectedParams {
+			testLiteralExpression(t,function.Parameters[i],ident)
+		}
+	}
+
+}
