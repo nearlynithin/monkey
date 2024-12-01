0c73d455b8a4a88ccf6643d1e02060ebebcc03b0
nearlynithin <nearlynithin@gmail.com>
2024-12-01 15:09:13 +0530

    feat: Tests for integer and boolean infix expressions

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -18,6 +18,16 @@ func TestEvalIntegerExpression(t *testing.T) {
 		{"8",8},
 		{"-8",-8},
 		{"-10",-10},
+		{"5 + 5 + 5 + 5 -10",10},
+		{"2 * 2 * 2 * 2 * 2",32},
+		{"-50 + 100 + -50",0},
+		{"5 * 2 + 10",20},
+		{"5 + 2 * 10",25},
+		{"20 + 2 * -10",0},
+		{"50 / 2 * 2 + 10",60},
+		{"2 * (5 + 10)",30},
+		{"3 * 3 * 3 + 10",37},
+		{"( 5 + 10 * 2 + 15 / 3) * 2 + -10",50},
 	}
 
 	for _, tt := range tests {
@@ -56,6 +66,21 @@ func TestBooleanExpression(t *testing.T) {
 	}{
 		{"true",true},
 		{"false",false},
+		{"1 < 2",true},
+		{"1 > 2",false},
+		{"2 == 2",true},
+		{"2 == 3",false},
+		{"2 != 3",true},
+		{"2 != 2",false},
+		{"true == true",true},
+		{"false == false",true},
+		{"true != false",true},
+		{"false != true",true},
+		{"true == false",false},
+		{"(1 > 2) == true",false},
+		{"(1 > 2) == false",true},
+		{"(1 < 2) == true",true},
+		{"(1 < 2) == false",false},
 	}
 
 	for _,tt := range tests {
