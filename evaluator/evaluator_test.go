8e012fb5de9a4f47c751cc4af8f5a712df94b9f3
nearlynithin <nearlynithin@gmail.com>
2024-12-02 11:24:54 +0530

    feat: Tests for If else expressions evalutation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -123,3 +123,35 @@ func TestBangOperator(t *testing.T) {
 		testBooleanObject(t,evaluated,tt.expected)
 	}
 }
+
+func TestIfElseExpressions(t *testing.T) {
+	tests := []struct{
+		input string
+		expected interface{}
+	}{
+		{"if (true) { 10 }", 10},
+		{"if (false) { 10 }", nil},
+		{"if (1<2) { 10 }", 10},
+		{"if (1>2) { 10 }", nil},
+		{"if (1<2) { 10 } else { 5 }", 10},
+		{"if (1>2) { 10 } else { 5 }", 5},
+	}
+
+	for _,tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t,evaluated,int64(integer))
+		} else {
+			testNullObject(t,evaluated)
+		}
+	}
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+	if obj != NULL {
+		t.Errorf("evaluated object is not NULL, got=%T (%+v)",obj,obj)
+		return false
+	}
+	return true
+}
