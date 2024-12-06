e6ad52b4a626312118ba0505e5b61e85a719b024
nearlynithin <nearlynithin@gmail.com>
2024-12-06 20:00:51 +0530

    feat: defining string object type for ast

--- object/object.go
+++ object/object.go
@@ -18,6 +18,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
 	FUNCTION_OBJ = "FUNCTION"
+	STRING_OBJ = "STRING"
 )
 
 type Object interface {
@@ -116,4 +117,16 @@ func (f *Function) Inspect() string {
 	out.WriteString("\n}")
 
 	return out.String()
+}
+
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjectType {
+	return STRING_OBJ
+}
+
+func (s * String) Inspect() string {
+	return s.Value
 }
