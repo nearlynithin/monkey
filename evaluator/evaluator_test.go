a1c11ff2d007414d2fca33ed7eebc4521ffd21ac
nearlynithin <nearlynithin@gmail.com>
2024-12-07 17:43:23 +0530

    feat: Tests for Builtin function evaluation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -332,3 +332,34 @@ func TestStringConcatenation(t *testing.T) {
 		t.Fatalf("str.Value is wrong, got=%q",str.Value)
 	}
 }
+
+func TestBuiltinFunctions(t *testing.T) {
+	tests := []struct{
+		input string
+		expected interface{}
+	}{
+		{`len("")`,0},
+		{`len("Hello World!")`,12},
+		{`len("four")`,4},
+		{`len(1)`,"argument to `len` not supported, got INTEGER"},
+		{`len("one","two")`,"wrong number of arguments. got=2, want=1"},
+	}
+
+	for _,tt := range tests {
+		evaluated := testEval(tt.input)
+
+		switch expected := tt.expected.(type) {
+		case int:
+			testIntegerObject(t,evaluated,int64(expected))
+		case string:
+			errObj, ok := evaluated.(*object.Error)
+			if !ok {
+				t.Fatalf("Object is not Error, got=%T(%+v)",evaluated,evaluated)
+			}
+			if errObj.Message != expected {
+				t.Fatalf("wrong error message = %q, expected=%q",errObj.Message,expected)
+			}
+		}
+	}
+
+}
